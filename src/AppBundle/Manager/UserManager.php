5e3ebd860fe404e2e63a9eb29dbab2d14d1548bb
Florian Le Gars <legars.florian@gmail.com>
2018-02-28 11:59:40 +0100

    Inscription (#26)
    * Form connexion
    
    * Formulaire de connexion
    
    * Formulaire de connexion
    
    * Ajout de l'inscription

--- src/AppBundle/Manager/UserManager.php
+++ src/AppBundle/Manager/UserManager.php
@@ -4,30 +4,26 @@ namespace AppBundle\Manager;
 use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
 
 class UserManager extends Controller
 {
 
     private $em;
-
-    public function __construct(EntityManagerInterface $entityManager)
-
+    private $passwordEncoder;
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->em = $entityManager;
+        $this->passwordEncoder = $passwordEncoder;
     }
-
-    public function getLesUsers()
+    public function createUser(User $user)
     {
-        return $this->em->getRepository(User::class)->findAll();
-    }
-
-    public function getUnUser($id)
-    {
-        return $this->em->getRepository(User::class)->find($id);
-    }
-
-    public function deleteUser($film){
-        $this->em->remove($film);
+        $password = $this->passwordEncoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($password);
+        $this->em->persist($user);
         $this->em->flush();
+        return $user;
     }
 }
